179ebb186467695abfbd33202073c5b92bc5dd76
WebH <max@webh.dev>
2022-08-21 09:12:36 +0100

    -

---
+++ script/script.js
@@ -0,0 +1,12 @@
+//Defining global variables
+
+const houseCost = document.getElementById("total-property-cost");
+const deposit = document.getElementById("deposit");
+const remainingMortgage = document.getElementById("remaining-mortgage");
+const interestRate = document.getElementById("interest-rate");
+const mortgageTerm = document.getElementById("length-of-mortgage");
+const monthlyMortgageCost = document.getElementById("monthly-mortgage-cost");
+const totalInterestAccrued = document.getElementById("total-interest-accrued");
+const totalInterestMonthly = document.getElementById("interest-per-month");
+const amountPayableOverMortgageTerm = document.getElementById("amount-payable-over-mortgage-term");
+
