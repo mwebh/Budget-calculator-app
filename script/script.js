79923f98e74bd2a098242e511486e50ec8c2587a
BitWebIT <106890622+BitWebIT@users.noreply.github.com>
2022-08-26 16:49:38 +0100

    -

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 //Defining global variables
 
+const page = document.querySelector('.page');
 const houseCost = document.getElementById("total-property-cost");
 const deposit = document.getElementById("deposit");
 const remainingMortgage = document.getElementById("remaining-mortgage");
@@ -9,4 +10,29 @@ const monthlyMortgageCost = document.getElementById("monthly-mortgage-cost");
 const totalInterestAccrued = document.getElementById("total-interest-accrued");
 const totalInterestMonthly = document.getElementById("interest-per-month");
 const amountPayableOverMortgageTerm = document.getElementById("amount-payable-over-mortgage-term");
+
+window.addEventListener('keyup', () => {
+
+    if (houseCost.value[0] === '£') {
+        houseCost.value.pop();
+        console.log(houseCost.value);
+    }
+
+
+    let rMortgage = (houseCost.value - deposit.value);
+    remainingMortgage.innerHTML = '£' + rMortgage.toLocaleString();
+9
+    let monthlyInterest = ((houseCost.value - deposit.value) * (interestRate.value / 100)) / 12;
+    totalInterestMonthly.innerHTML = '£' + monthlyInterest.toLocaleString();
+
+    let totalInterest = ((houseCost.value - deposit.value) * ((interestRate.value / 100) /12)) * mortgageTerm.value;
+    totalInterestAccrued.innerHTML = '£' + totalInterest.toLocaleString();
+
+    let mMortgageCost = ((((houseCost.value - deposit.value) / mortgageTerm.value) / 12) + monthlyInterest);
+    monthlyMortgageCost.innerHTML = '£' + mMortgageCost.toLocaleString();
+
+    let totalAmountPayable = ((houseCost.value - deposit.value) + totalInterest)
+    amountPayableOverMortgageTerm.innerHTML = '£' + totalAmountPayable.toLocaleString();
+
+})
 
